244016eabfdfffb8943a77d019079ec0b85c6fc5
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2022-12-01 20:08:13 +0100

    fix: sign before verify

--- run.sh
+++ run.sh
@@ -6,11 +6,11 @@ python sign-util-v2.py -v -c data/usercert.pem -a data/root.pem -m data/mime_fil
 
 echo ""
 
-echo "======== SHOULD BE GOOD, WE DON'T CHECK CRL ========"
-python sign-util-v2.py -v -c data/revokatedclientcert.pem -a data/root.pem -m data/mime_file_output_r.eml
-
-echo ""
-
 echo "======== SHOULD BE BAD, CERTIFICATE INSIDE CRL ========"
 python sign-util-v2.py -s -c data/revokatedclientcert.pem -p data/revokatedclientkey.pem -i data/file_to_sign.txt -m data/mime_file_output_r.eml
 python sign-util-v2.py -v -c data/revokatedclientcert.pem -a data/root.pem -m data/mime_file_output_r.eml -r data/crl/crlist.pem
+
+echo ""
+
+echo "======== SHOULD BE GOOD, WE DON'T CHECK CRL ========"
+python sign-util-v2.py -v -c data/revokatedclientcert.pem -a data/root.pem -m data/mime_file_output_r.eml
